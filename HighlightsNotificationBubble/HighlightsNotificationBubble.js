785cbe7d9f5ac63cdcc8e2b8dc7993ad7d50fe43
Dorumin <dorminsama@gmail.com>
2019-02-21 07:04:23 -0300

    Update HighlightsNotificationBubble

--- HighlightsNotificationBubble/HighlightsNotificationBubble.js
+++ HighlightsNotificationBubble/HighlightsNotificationBubble.js
@@ -1,54 +1,81 @@
 /* HighlightsNotificationBubble
  *
  * Provides different ways to style the bubble on the notification icons relative to the current highlights
+ * *ahem* Did I say highlights? I meant, announcements! Of course, how could I forget? Those things that follow you around everywhere and you can't dismiss without a script? Ah, they're my favorite
  *
- * @scope Personal
  * @author Dorumin
  */
  
-(function highlightsNotificationBubbleInit(target) {
-    if (!window.MutationObserver || !target) return;
-    var config = window.HighlightsNotificationBubble || {
+(function() {
+    if (
+        !window.MutationObserver ||
+        (window.HighlightsNotificationBubble && HighlightsNotificationBubble.init)
+    ) return;
+ 
+    window.HighlightsNotificationBubble = $.extend({
+        observer: null,
+        target: document.getElementById('globalNavigation'),
+        id: mw.config.get('wgCityId'),
+        config: {
+            attributes: true,
+            childList: true,
+            characterData: true,
+            subtree: true // Infinite loops shall not pass
+        },
         all: {
             color: '#fff',
-            bg_color: '#c5ff19'
+            background: '#ffc500'
+        },
+        // Yes, this returns if 0 == 0, but who cares?
+        getStyles: function(all, special) {
+            var navigation = document.getElementById('globalNavigation'),
+            highlights = navigation.querySelectorAll(special),
+            notifications = navigation.querySelectorAll(all);
+ 
+            if (highlights.length == notifications.length) {
+                return this.all;
+            } else if (highlights.length) {
+                return this.any;
+            } else {
+                return this.none;
+            }
+        },
+        styleBubble(bubble, styles) {
+            if (!styles) return;
+ 
+            // Silly past me, all in for snake_case and willing to show it off at any opportunity
+            styles.background = styles.background || styles.bg_color;
+ 
+            for (var property in styles) {
+                var value = styles[property];
+                if (bubble.style[property] != value) {
+                    bubble.style[property] = value;
+                }
+            }
+        },
+        // Calls twice after changing colors but not like that matters
+        onMutation: function() {
+            var announcementsBubble = document.getElementById('onSiteNotificationsCount'),
+            highlightsBubble = document.querySelector('#notificationsEntryPoint .wds-global-navigation__notifications-counter');
+ 
+            this.styleBubble(announcementsBubble, this.getStyles(
+                    '.wds-notification-card.wds-is-unread',
+                    '.wds-notification-card.wds-is-unread[data-type="announcement"]'
+            ));
+ 
+            this.styleBubble(highlightsBubble, this.getStyles(
+                '#notificationsContainer li[data-wiki-id="' + this.id + '"] .notification.unread',
+                '#notificationsContainer li[data-wiki-id="' + this.id + '"] .notification.unread.admin-notification'
+            ));
+        },
+        init: function() {
+            this.observer = new MutationObserver(this.onMutation.bind(this));
+ 
+            this.observer.observe(this.target, this.config);
+ 
+            this.onMutation();
         }
-    };
+    }, window.HighlightsNotificationBubble);
  
-    function on_mutation(does_it_really_matter_tho) {
-        var bubbles = document.querySelector('#notificationsEntryPoint .bubbles');
-        if (!bubbles || bubbles.className != 'bubbles show') return;
-        var notification_list = document.querySelector('#notificationsContainer [data-wiki-id="' + wgCityId + '"]');
-        if (!notification_list) return;
-        var all_unread = notification_list.querySelectorAll('.unread'),
-        all_highlights = notification_list.querySelectorAll('.unread.admin-notification');
-        if (!all_unread || !all_highlights) return;
-        if (config.all && all_highlights.length && all_unread.length == all_highlights.length) {
-            add_styles('all');
-        } else if (config.any && all_highlights.length) {
-            add_styles('any');
-        } else if (config.none && !all_highlights.length) {
-            add_styles('none');
-        } else {
-            document.querySelector('.wds-global-navigation__notifications-menu-counter').removeAttribute('style');
-        }
-    }
- 
-    function add_styles(mode) {
-        var counter = document.querySelector('.wds-global-navigation__notifications-menu-counter');
-        counter.removeAttribute('style');
-        counter.style.color = config[mode].color;
-        counter.style.backgroundColor = config[mode].bg_color;
-    }
- 
-    var observer = new MutationObserver(on_mutation);
- 
-    observer.observe(target, {
-        attributes: true,
-        childList: true,
-        characterData: true,
-        subtree: true // Don't worry, no chance of infinite loops here... I hope
-    });
- 
-    on_mutation('no it does not matter');
-})(document.getElementById('GlobalNavigationWallNotifications'));
+    HighlightsNotificationBubble.init();
+})();
